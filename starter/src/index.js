89e23199321598757842aa94aa70416daee1d339
Huynh Uyen <uyenhuynh13122000@gmail.com>
2023-12-03 12:33:33 +0700

    store.js

--- starter/src/index.js
+++ starter/src/index.js
@@ -1,13 +1,15 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { store } from "./store";
+import { Provider } from "react-redux";
 
-const container = document.getElementById('root');
-const root = createRoot(container);
-
-root.render(
+ReactDOM.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
